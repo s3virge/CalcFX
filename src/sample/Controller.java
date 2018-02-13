d22b9888789f9cf47432993024c5b7cf87382c28
s3virge <kobzarvit@gmail.com>
2018-02-13 18:11:27 +0200

    Update Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,4 +3,11 @@ package sample;
 public class Controller {
   private float fResultValue = 0;
   private float fDisplayValue = 0;
+  
+  private void plus() {
+  //получить текст из поля ввода
+  //преобразовать в float
+    //прибавить его к fResultValue
+    //вывести новое значение fResultVal в поле ввода
+  }
 }
