9c97281dc369dd42f119303f0b4562e5032dd606
s3virge <kobzarvit@gmail.com>
2018-02-14 08:27:10 +0200

    add some functions to Calculator class

--- src/sample/Calculator.java
+++ src/sample/Calculator.java
@@ -1,4 +1,36 @@
 package sample;
 
 public class Calculator {
+    private double fResultValue = 0;
+
+    public double clean() {
+        System.out.println("Button Clean was pressed.");
+        return fResultValue = 0;
+    }
+
+    public double backspace() {
+        System.out.println("Button Return was pressed.");
+        return 0;
+    }
+
+    private double plus(double val) {
+        System.out.println("Button plus was pressed.");
+        return fResultValue += val;
+    }
+
+    private double minus(double val){
+        return fResultValue -= val;
+    }
+
+    private double multiply(double val) {
+        return fResultValue *= val;
+    }
+
+    private double divide(double val) {
+        return fResultValue /= val;
+    }
+
+    private double equals(double val) {
+        return fResultValue;
+    }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,33 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
-  private float fResultValue = 0;
-  private float fDisplayValue = 0;
-  
-  private void plus() {
-  //получить текст из поля ввода
-  //преобразовать в float
-    //прибавить его к fResultValue
-    //вывести новое значение fResultVal в поле ввода
-  }
+
+    @FXML
+    private TextField tfDispley;
+
+    private Calculator calc = new Calculator();
+
+      public void onActionKeyPress(ActionEvent actionEvent) {
+        processKeyPress((Button) actionEvent.getSource());
+    }
+
+    public void onDigitKeyPress(ActionEvent actionEvent) {
+    }
+
+    private void processKeyPress(Button clickedBtn) {
+        switch (clickedBtn.getId()) {
+            case "btnClean":
+                tfDispley.setText(String.format("%f", calc.clean()));
+                break;
+
+            case "btnBackspace":
+                calc.backspace();
+                break;
+        }
+    }
 }
